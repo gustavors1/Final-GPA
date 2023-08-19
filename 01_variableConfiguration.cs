0d99fa2bf3a86c01db77c4e9eaf47fef69e6c83d
gusta <gustavoandres_r@hotmail.com>
2023-08-19 20:38:23 +0200

    Almacenamiento de los valores numéricos de calificación para cada curso

---
+++ 01_variableConfiguration.cs
@@ -0,0 +1,38 @@
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer science I";
+string course5Name = "Psycology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+// Variables que almacenan las calificaciones de cada curso
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+// Nombre del curso junto con calificación numérica
+
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+/* 
+Este código usa el signo $ para representar las variables 
+course1Name, course1Grade y course1Credit. 
+Cuando el código se ejecuta, las variables se reemplazan por sus valores 
+y el resultado se imprime en la consola.
+*/
